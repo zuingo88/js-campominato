6b4d5edf4f661e9b614b4d94c8bd99a829e045dd
valerio sgura <valeriosgura88@gmail.com>
2021-03-15 15:22:43 +0100

    fine bonus

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function numberRnd(min, max) {
 
 };
 
-function esercizio() {
+function livelloZero() {
 
     var arrLoserNumbers = [];
 
@@ -72,4 +72,143 @@ function esercizio() {
 
 };
 
-//esercizio();
+function livelloUno() {
+
+    var arrLoserNumbers = [];
+
+    while (arrLoserNumbers.length < 16) {
+
+        var loserNumber = numberRnd(1, 79);
+
+        if (arrLoserNumbers.indexOf(loserNumber) === -1) {
+
+            arrLoserNumbers.push(loserNumber);
+        }
+
+        arrLoserNumbers.sort(function (a, b) {
+            return a - b;
+        });
+    }
+
+    console.log(arrLoserNumbers);
+
+    var arrPlayerNumbers = [];
+    var arrLoser = [];
+    var score = 0;
+    var titolo = document.getElementById('titolo');
+    var sottoTitolo = document.getElementById('accadue');
+    var sottoTitolo1 = document.getElementById('accatre');
+
+    for (var i = 0; i < (80 - 16); i++) {
+
+        var playerNumber = parseInt(prompt('Ineserisci un numero'));
+
+        if ((arrPlayerNumbers.indexOf(playerNumber) === -1) && (!arrLoserNumbers.includes(playerNumber)) && ((playerNumber > 0) && (playerNumber < 80))) {
+
+            arrPlayerNumbers.push(playerNumber);
+            score += 1;
+            titolo.innerHTML = 'Il tuo punteggio: ' + score;
+            sottoTitolo.innerHTML = 'I tuoi numeri: ' + arrPlayerNumbers;
+
+            if (score == (80 - 16)) {
+
+                alert('COMPLIMENTI, HAI FINITO IL GIOCO!!!');
+                titolo.innerHTML = 'GIOCO COMPLETATO!';
+
+            }
+
+        } else {
+
+            arrLoser.push(playerNumber);
+            alert('Hai perso!!! muhahahaha');
+            titolo.innerHTML = 'Hai totalizzato ' + score + ' punti';
+            sottoTitolo1.innerHTML = 'Numero perdente: ' + arrLoser;
+            break;
+
+        }
+    }
+
+};
+
+function livelloDue() {
+
+    var arrLoserNumbers = [];
+
+    while (arrLoserNumbers.length < 16) {
+
+        var loserNumber = numberRnd(1, 49);
+
+        if (arrLoserNumbers.indexOf(loserNumber) === -1) {
+
+            arrLoserNumbers.push(loserNumber);
+        }
+
+        arrLoserNumbers.sort(function (a, b) {
+            return a - b;
+        });
+    }
+
+    console.log(arrLoserNumbers);
+
+    var arrPlayerNumbers = [];
+    var arrLoser = [];
+    var score = 0;
+    var titolo = document.getElementById('titolo');
+    var sottoTitolo = document.getElementById('accadue');
+    var sottoTitolo1 = document.getElementById('accatre');
+
+    for (var i = 0; i < (50 - 16); i++) {
+
+        var playerNumber = parseInt(prompt('Ineserisci un numero'));
+
+        if ((arrPlayerNumbers.indexOf(playerNumber) === -1) && (!arrLoserNumbers.includes(playerNumber)) && ((playerNumber > 0) && (playerNumber < 50))) {
+
+            arrPlayerNumbers.push(playerNumber);
+            score += 1;
+            titolo.innerHTML = 'Il tuo punteggio: ' + score;
+            sottoTitolo.innerHTML = 'I tuoi numeri: ' + arrPlayerNumbers;
+
+            if (score == (50 - 16)) {
+
+                alert('COMPLIMENTI, HAI FINITO IL GIOCO!!!');
+                titolo.innerHTML = 'GIOCO COMPLETATO!';
+
+            }
+
+        } else {
+
+            arrLoser.push(playerNumber);
+            alert('Hai perso!!! muhahahaha');
+            titolo.innerHTML = 'Hai totalizzato ' + score + ' punti';
+            sottoTitolo1.innerHTML = 'Numero perdente: ' + arrLoser;
+            break;
+
+        }
+    }
+
+};
+
+
+function gioco() {
+
+    alert('VOGLIO FARE UN GIOCO CON TE...');
+
+    var level = parseInt(prompt('Scegli un livello da 0 a 2'));
+
+    if (level == 0) {
+        alert('LIVELLO 0: scegli numeri tra 0 e 100 (esclusi)');
+        livelloUno();
+    } else if (level == 1) {
+        alert('LIVELLO 1: scegli numeri tra 0 e 80 (esclusi)');
+        livelloUno();
+    } else if (level == 2) {
+        alert('LIVELLO 2: scegli numeri tra 0 e 50 (esclusi)');
+        livelloDue();
+    } else {
+        alert('Si vabbè gioca da solo allora dai...fai tu, sentiti libero');
+    }
+
+};
+
+//gioco();
+
