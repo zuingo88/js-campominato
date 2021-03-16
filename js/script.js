a229978a0f9bd4559b94bd2e8bba9669e842c7e4
valerio sgura <valeriosgura88@gmail.com>
2021-03-16 17:53:04 +0100

    inserimento button start

--- js/script.js
+++ js/script.js
@@ -9,206 +9,88 @@
 
 function numberRnd(min, max) {
 
-    var value = Math.floor(Math.random() * (max - min) + min); //max e min sono inclusi
+    var value = Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
     return value;
-
 };
-
-function livelloZero() {
-
-    var arrLoserNumbers = [];
-
-    while (arrLoserNumbers.length < 16) {
-
-        var loserNumber = numberRnd(1, 99);
-
-        if (arrLoserNumbers.indexOf(loserNumber) === -1) {
-
-            arrLoserNumbers.push(loserNumber);
-        }
-
-        arrLoserNumbers.sort(function (a, b) {
-            return a - b;
-        });
-    }
-
-    console.log(arrLoserNumbers);
-
-    var arrPlayerNumbers = [];
-    var arrLoser = [];
-    var score = 0;
-    var titolo = document.getElementById('titolo');
-    var sottoTitolo = document.getElementById('accadue');
-    var sottoTitolo1 = document.getElementById('accatre');
-
-    for (var i = 0; i < (100 - 16); i++) {
-
-        var playerNumber = parseInt(prompt('Ineserisci un numero'));
-
-        if ((arrPlayerNumbers.indexOf(playerNumber) === -1) && (!arrLoserNumbers.includes(playerNumber)) && ((playerNumber > 0) && (playerNumber < 100))) {
-
-            arrPlayerNumbers.push(playerNumber);
-            score += 1;
-            titolo.innerHTML = 'Il tuo punteggio: ' + score;
-            sottoTitolo.innerHTML = 'I tuoi numeri: ' + arrPlayerNumbers;
-
-            if (score == (100 - 16)) {
-
-                alert('COMPLIMENTI, HAI FINITO IL GIOCO!!!');
-                titolo.innerHTML = 'GIOCO COMPLETATO!';
-
-            }
-
-        } else {
-
-            arrLoser.push(playerNumber);
-            alert('Hai perso!!! muhahahaha');
-            titolo.innerHTML = 'Hai totalizzato ' + score + ' punti';
-            sottoTitolo1.innerHTML = 'Numero perdente: ' + arrLoser;
-            break;
-
-        }
-    }
-
-};
-
-function livelloUno() {
-
-    var arrLoserNumbers = [];
-
-    while (arrLoserNumbers.length < 16) {
-
-        var loserNumber = numberRnd(1, 79);
-
-        if (arrLoserNumbers.indexOf(loserNumber) === -1) {
-
-            arrLoserNumbers.push(loserNumber);
-        }
-
-        arrLoserNumbers.sort(function (a, b) {
-            return a - b;
-        });
-    }
-
-    console.log(arrLoserNumbers);
-
-    var arrPlayerNumbers = [];
-    var arrLoser = [];
-    var score = 0;
-    var titolo = document.getElementById('titolo');
-    var sottoTitolo = document.getElementById('accadue');
-    var sottoTitolo1 = document.getElementById('accatre');
-
-    for (var i = 0; i < (80 - 16); i++) {
-
-        var playerNumber = parseInt(prompt('Ineserisci un numero'));
-
-        if ((arrPlayerNumbers.indexOf(playerNumber) === -1) && (!arrLoserNumbers.includes(playerNumber)) && ((playerNumber > 0) && (playerNumber < 80))) {
-
-            arrPlayerNumbers.push(playerNumber);
-            score += 1;
-            titolo.innerHTML = 'Il tuo punteggio: ' + score;
-            sottoTitolo.innerHTML = 'I tuoi numeri: ' + arrPlayerNumbers;
-
-            if (score == (80 - 16)) {
-
-                alert('COMPLIMENTI, HAI FINITO IL GIOCO!!!');
-                titolo.innerHTML = 'GIOCO COMPLETATO!';
-
-            }
-
-        } else {
-
-            arrLoser.push(playerNumber);
-            alert('Hai perso!!! muhahahaha');
-            titolo.innerHTML = 'Hai totalizzato ' + score + ' punti';
-            sottoTitolo1.innerHTML = 'Numero perdente: ' + arrLoser;
-            break;
-
-        }
-    }
-
-};
-
-function livelloDue() {
-
-    var arrLoserNumbers = [];
-
-    while (arrLoserNumbers.length < 16) {
-
-        var loserNumber = numberRnd(1, 49);
-
-        if (arrLoserNumbers.indexOf(loserNumber) === -1) {
-
-            arrLoserNumbers.push(loserNumber);
-        }
-
-        arrLoserNumbers.sort(function (a, b) {
-            return a - b;
-        });
-    }
-
-    console.log(arrLoserNumbers);
-
-    var arrPlayerNumbers = [];
-    var arrLoser = [];
-    var score = 0;
-    var titolo = document.getElementById('titolo');
-    var sottoTitolo = document.getElementById('accadue');
-    var sottoTitolo1 = document.getElementById('accatre');
-
-    for (var i = 0; i < (50 - 16); i++) {
-
-        var playerNumber = parseInt(prompt('Ineserisci un numero'));
-
-        if ((arrPlayerNumbers.indexOf(playerNumber) === -1) && (!arrLoserNumbers.includes(playerNumber)) && ((playerNumber > 0) && (playerNumber < 50))) {
-
-            arrPlayerNumbers.push(playerNumber);
-            score += 1;
-            titolo.innerHTML = 'Il tuo punteggio: ' + score;
-            sottoTitolo.innerHTML = 'I tuoi numeri: ' + arrPlayerNumbers;
-
-            if (score == (50 - 16)) {
-
-                alert('COMPLIMENTI, HAI FINITO IL GIOCO!!!');
-                titolo.innerHTML = 'GIOCO COMPLETATO!';
-
-            }
-
-        } else {
-
-            arrLoser.push(playerNumber);
-            alert('Hai perso!!! muhahahaha');
-            titolo.innerHTML = 'Hai totalizzato ' + score + ' punti';
-            sottoTitolo1.innerHTML = 'Numero perdente: ' + arrLoser;
-            break;
-
-        }
-    }
-
-};
-
 
 function gioco() {
 
     alert('VOGLIO FARE UN GIOCO CON TE...');
 
-    var level = parseInt(prompt('Scegli un livello da 0 a 2'));
+    var level = parseInt(prompt('Scegli un livello tra 0, 1 e 2'));
+    var maxRnd;
 
-    if (level == 0) {
-        alert('LIVELLO 0: scegli numeri tra 0 e 100 (esclusi)');
-        livelloUno();
-    } else if (level == 1) {
-        alert('LIVELLO 1: scegli numeri tra 0 e 80 (esclusi)');
-        livelloUno();
-    } else if (level == 2) {
-        alert('LIVELLO 2: scegli numeri tra 0 e 50 (esclusi)');
-        livelloDue();
-    } else {
-        alert('Si vabbè gioca da solo allora dai...fai tu, sentiti libero');
+    switch (level) {
+        case 0:
+            maxRnd = 100;
+            break;
+
+        case 1:
+            maxRnd = 80;
+            break;
+
+        case 2:
+            maxRnd = 50;
+            break;      
     }
 
+    var arrLoserNumbers = [];
+
+    while (arrLoserNumbers.length < 16) {
+
+        var loserNumber = numberRnd(1, maxRnd);
+
+        if (arrLoserNumbers.indexOf(loserNumber) === -1) {
+
+            arrLoserNumbers.push(loserNumber);
+        }
+
+        arrLoserNumbers.sort(function (a, b) {
+            return a - b;
+        });
+    }
+
+    console.log(arrLoserNumbers);
+
+    var maxRnd;
+    var arrPlayerNumbers = [];
+    var arrLoser = [];
+    var score = 0;
+    var titolo = document.getElementById('titolo');
+    var sottoTitolo = document.getElementById('accadue');
+    var sottoTitolo1 = document.getElementById('accatre');
+
+    for (var i = 0; i < (maxRnd - 16); i++) {
+
+        var playerNumber = parseInt(prompt('Ineserisci un numero'));
+
+        if ((arrPlayerNumbers.indexOf(playerNumber) === -1) && (arrLoserNumbers.indexOf(playerNumber) === -1) && ((playerNumber > 0) && (playerNumber < maxRnd))) {  //condizioni: playerNumber non ripetuto, playerNumber non contenuto in arrLoserNumber, compreso tra 0 e 100 valori esclusi
+
+
+            arrPlayerNumbers.push(playerNumber);
+            score += 1;
+            titolo.innerHTML = 'Il tuo punteggio: ' + score;
+            sottoTitolo.innerHTML = 'I tuoi numeri: ' + arrPlayerNumbers;
+
+            if (score == (maxRnd - 16)) {
+
+                alert('COMPLIMENTI, HAI FINITO IL GIOCO!!!');
+                titolo.innerHTML = 'GIOCO COMPLETATO!';
+            }
+
+        } else {
+
+            arrLoser.push(playerNumber);
+            alert('Hai perso!!! muhahahaha');
+            titolo.innerHTML = 'Hai totalizzato ' + score + ' punti';
+            sottoTitolo1.innerHTML = 'Numero perdente: ' + arrLoser;
+            break;
+        }
+    } //chiusura for
 };
+
+var startBtn = document.getElementById('start');
+startBtn.addEventListener('click', gioco);
 
 //gioco();
 
